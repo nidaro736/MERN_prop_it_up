f5caeb89e8e4587de36ca85d1e7c0a34cd1bb173
Nicholas Rodgers <nidaro736@Nicholass-MacBook-Pro.local>
2020-03-06 10:56:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/PersonCard';
+import PersonCard from './components/PersonCard';
 
-function App() {
+const App = () =>
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard fName = "Jane" lName = "Doe" age = {45} hairColor = "Black" />
+      <PersonCard fName = "John" lName = "Smith" age = {88} hairColor = "Brown" />
+      <PersonCard fName = "Millard" lName = "Fillmore" age = {50} hairColor = "Brown" />
+      <PersonCard fName = "Maria" lName = "Smith" age = {62} hairColor = "Brown" />      
     </div>
   );
 }
